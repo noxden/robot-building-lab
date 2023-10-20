8da322dae7d9a76dc85d0972657990ae7f259163
Elias <elbobo@live.de>
2023-10-21 01:17:27 +0200

    whiteboxlevel + list scripts

---
+++ Robot Building Lab/Assets/Scripts/AttachToListSpeed.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachToListSpeed : MonoBehaviour
+{
+    public void attach() 
+    {
+        //transform.root.GetComponent<RobotController>().AccelerationWheels.Add(gameObject.GetComponent<WheelCollider>());
+    }
+    public void detach()
+    {
+        //transform.root.GetComponent<RobotController>().AccelerationWheels.Remove(gameObject.GetComponent<WheelCollider>());
+    }
+}
---
+++ Robot Building Lab/Assets/Scripts/AttachToListSteering.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachToListSteering : MonoBehaviour
+{
+    public void attach()
+    {
+        //transform.root.GetComponent<RobotController>().SteeringWheels.Add(gameObject.GetComponent<WheelCollider>());
+    }
+    public void detach()
+    {
+        //transform.root.GetComponent<RobotController>().SteeringWheels.Remove(gameObject.GetComponent<WheelCollider>());
+    }
+}
